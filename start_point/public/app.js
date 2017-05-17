7c1d4f54ee90321237f807b1f6bac3412452fb7d
Euan Fullarton <euanfullarton7@gmail.com>
2017-05-17 17:34:38 +0100

    Managed to return something from the api

---
+++ start_point/public/app.js
@@ -0,0 +1,37 @@
+var app = function(){
+  var url = "https://api.spotify.com/v1/search?q=christmas&type=album";
+      makeRequest(url, requestComplete);
+    }
+
+    var populateList = function(albums){
+      console.log(albums[0].name);
+      var display = document.getElementById('albums');
+
+      albums.forEach(function(album){
+        display.innerText = album.name;
+      });
+    }
+
+    var makeRequest = function(url, callback){
+      var request = new XMLHttpRequest();
+      request.open("GET", url);
+      request.addEventListener('load', callback);
+      request.send();
+    }
+
+    var requestComplete = function(){
+        if(this.status !== 200) return;
+        var jsonString = this.responseText;
+        var albums = JSON.parse(jsonString);
+
+        populateList(albums.albums.items);
+      }  
+
+
+
+
+
+window.addEventListener('load', app);
+
+
+// obj.albums.items
